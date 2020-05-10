2efaa08eb430c23e2ff1d6c9763c79f353c2b98b
MahdiSarani <37251741+mahdisarani@users.noreply.github.com>
2020-05-10 14:27:54 +0430

    Add files via upload

---
+++ templates/payment-error.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Prevent loading this file directly
+ */
+defined( 'ABSPATH' ) || exit();
+?>
+
+<?php $settings = LP()->settings; ?>
+
+<div class="learn-press-message error ">
+	<div><?php echo __( 'Transation failed', 'learnpress-payping' ); ?></div>		
+</div>
